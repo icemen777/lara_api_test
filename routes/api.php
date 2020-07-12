0f51e64078c14c81d0f04ae540b4ec04d485aca9
icemen777 <mentor7771@gmail.com>
2020-07-12 13:32:09 +0300

    Change to small letters

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('members', 'Api\MemberController');
+Route::apiResource('members', 'API\MemberController');
 
-Route::get('event_members/{id}', ['uses'=>'Api\MemberController@eventMembers', 'as'=>'eventMembers' ]);
+Route::get('event_members/{id}', ['uses'=>'API\MemberController@eventMembers', 'as'=>'eventMembers' ]);
